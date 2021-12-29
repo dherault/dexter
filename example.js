7e644fec92fce20213f8d1c1c4b9315eb0490648
David Hérault <dherault@gmail.com>
2021-12-29 10:01:46 +0100

    Adapt example

--- example.js
+++ example.js
@@ -15,18 +15,29 @@ async function main() {
   let wrappedNativePriceUSD
 
   await sushiswap.addStablecoinsOracleListener(({ timestamp, priceUSD }) => {
-    console.log('Wrapped native USD', timestamp, priceUSD.toString())
+    console.log('MATIC and WMATIC $ price:', timestamp, priceUSD.toString())
 
     wrappedNativePriceUSD = priceUSD
   })
 
+  const wmaticAddress = sushiswap.getToken('WMATIC').address
   const wethAddress = sushiswap.getToken('WETH').address
   const sushiAddress = sushiswap.getToken('SUSHI').address
 
+  const wmaticWethPairAddress = await sushiswap.getPairAddress(wmaticAddress, wethAddress)
+  const wmaticSushiPairAddress = await sushiswap.getPairAddress(wmaticAddress, sushiAddress)
   const wethSushiPairAddress = await sushiswap.getPairAddress(wethAddress, sushiAddress)
 
+  await sushiswap.addOracleListener(wmaticWethPairAddress, data => {
+    console.log('WMATIC-WETH:', data.timestamp, data[wmaticAddress].price.toString(), data[wethAddress].price.toString())
+    console.log('WETH $ price:', data[wethAddress].price.times(wrappedNativePriceUSD).toString())
+  })
+  await sushiswap.addOracleListener(wmaticSushiPairAddress, data => {
+    console.log('WMATIC-SUSHI:', data.timestamp, data[wmaticAddress].price.toString(), data[sushiAddress].price.toString())
+    console.log('SUSHI $ price:', data[sushiAddress].price.times(wrappedNativePriceUSD).toString())
+  })
   await sushiswap.addOracleListener(wethSushiPairAddress, data => {
-    console.log('WETH-SUSHI', data.timestamp, data[wethAddress].price.toString(), data[sushiAddress].price.toString())
+    console.log('WETH-SUSHI:', data.timestamp, data[wethAddress].price.toString(), data[sushiAddress].price.toString())
   })
 }
 
